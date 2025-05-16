c571e80223a8c24f9a1ed7054e84c47cf85c91c1
daniel silva <daniel.silva@alboompro.com>
2025-05-16 16:29:58 -0300

    :sparkles: implement DynamicSkyDome for enhanced environment rendering

--- src/components/Flipbook/components/FlipbookScene/FlipbookEnvironment.tsx
+++ src/components/Flipbook/components/FlipbookScene/FlipbookEnvironment.tsx
@@ -1,22 +1,58 @@
-import { Environment } from "@react-three/drei";
 import { Light } from "./Light";
 import { FlipbookEnvironmentProps } from "../../types";
-import { useMemo } from "react";
+import { useEffect, useMemo, useRef } from "react";
+import { Group } from "three";
+import { useLoader } from "@react-three/fiber";
+import * as THREE from "three";
+
+const scale = 50;
+
+export const DynamicSkyDome = ({
+  environmentUrl,
+}: {
+  environmentUrl: string;
+}) => {
+  const texture = useLoader(THREE.TextureLoader, environmentUrl);
+
+  useEffect(() => {
+    // Configura a textura para mapeamento equiretangular
+    texture.mapping = THREE.EquirectangularReflectionMapping;
+    texture.colorSpace = "srgb";
+    texture.center.set(0.5, 0.5);
+    texture.rotation = Math.PI;
+    texture.flipY = true;
+  }, [texture]);
+
+  // Calcula a escala base uma única vez
+
+  return (
+    <mesh scale={[scale, -scale, scale]}>
+      <sphereGeometry args={[1, 128, 128]} />
+      <meshBasicMaterial
+        map={texture}
+        side={THREE.BackSide}
+        toneMapped={false}
+      />
+    </mesh>
+  );
+};
 
 export const FlipbookEnvironment = ({
   environmentUrl,
 }: FlipbookEnvironmentProps) => {
-  return useMemo(
-    () => (
-      <group>
-        <Light />
-        <Environment
-          preset={environmentUrl ? undefined : "studio"}
-          files={environmentUrl}
-          background={!!environmentUrl}
-        />
-      </group>
-    ),
+  const groupRef = useRef<Group>(null);
+
+  const environment = useMemo(
+    () => environmentUrl && <DynamicSkyDome environmentUrl={environmentUrl} />,
     [environmentUrl]
+  );
+
+  const light = useMemo(() => <Light />, []);
+
+  return (
+    <group ref={groupRef}>
+      {light}
+      {environment}
+    </group>
   );
 };
